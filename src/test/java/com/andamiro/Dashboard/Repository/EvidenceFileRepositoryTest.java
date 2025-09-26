a442f94e90e9f34d86cd6a0f69445a41044a6d9e
ykm20651 <ykm20651@gmail.com>
2025-09-26 13:21:09 +0900

    CHORE : 각 레이어별 단위 테스트 환경 파일 생성

---
+++ src/test/java/com/andamiro/Dashboard/Repository/EvidenceFileRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.andamiro.Dashboard.Repository;
+
+public class EvidenceFileRepositoryTest {
+}
---
+++ src/test/java/com/andamiro/Dashboard/Service/UserServiceTest.java
@@ -0,0 +1,4 @@
+package com.andamiro.Dashboard.Service;
+
+public class UserServiceTest {
+}
