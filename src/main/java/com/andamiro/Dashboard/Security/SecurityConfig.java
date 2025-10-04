1c87df3101aa456b2ed8ee19d41e3e06ab94ee75
ykm20651 <ykm20651@gmail.com>
2025-10-04 17:17:41 +0900

    fix : cors 설정 시 포트번호 제거

--- src/main/java/com/andamiro/Dashboard/Security/SecurityConfig.java
+++ src/main/java/com/andamiro/Dashboard/Security/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://15.164.99.177:80");
+        configuration.addAllowedOrigin("http://15.164.99.177");
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
