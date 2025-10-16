5abfe5ce47aac49705659a442c83c656076edcfe
ykm20651 <ykm20651@gmail.com>
2025-10-16 20:15:03 +0900

    feat : 보고서 삭제 API 개발 + FastAPI 연동 + 프론트 fetch url 경로 재설정

--- src/main/java/com/andamiro/Dashboard/Dto/ReportDTO/FastApiReportRequest.java
+++ src/main/java/com/andamiro/Dashboard/Dto/ReportDTO/FastApiReportRequest.java
@@ -5,5 +5,20 @@ public record FastApiReportRequest(
         String description,
         String location,
         String report_type,
-        String language
-) {}
+        String language,
+        
+        Boolean use_rag,
+        String collection,
+        Integer top_k,
+        String model,
+        String title
+) {
+    public FastApiReportRequest { //null로 들어온 값에 대해 내부적으로 기본값을 주기.
+        if (use_rag == null) use_rag = true;
+        if (collection == null) collection = "marine_laws";
+        if (top_k == null) top_k = 5;
+        if (model == null) model = "gpt-4o-mini";
+        if (title == null) title = "해양 보험 청구 보고서";
+    }
+
+}
