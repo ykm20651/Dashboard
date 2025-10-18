7e7f799f48cadb176df7a3ff1470443dcb2e98e2
ykm20651 <ykm20651@gmail.com>
2025-10-18 23:36:25 +0900

    fix : ec2 파일 경로 접근 허용

--- src/main/java/com/andamiro/Dashboard/Config/WebConfig.java
+++ src/main/java/com/andamiro/Dashboard/Config/WebConfig.java
@@ -17,4 +17,26 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 증거자료 파일 접근 허용
+        registry.addResourceHandler("/files/evidence/**")
+                .addResourceLocations("file:/home/ec2-user/Dashboard/uploads/evidence/");
+
+        // AI 보고서 등 다른 파일도 함께 처리 가능
+        registry.addResourceHandler("/files/reports/**")
+                .addResourceLocations("file:/home/ec2-user/Dashboard/uploads/reports/");
+    }
+
+    /*
+    1. 물리적 파일 저장 경로 (EC2 서버):
+        ->증거자료: /home/ec2-user/Dashboard/uploads/evidence/
+        ->AI 보고서: /home/ec2-user/Dashboard/uploads/reports/
+
+    2. DB 저장 경로 (URL 형태):
+        ->증거자료: /files/evidence/파일명
+        ->AI 보고서: /files/reports/파일명
+
+    */
 }
